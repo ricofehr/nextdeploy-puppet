737927a72f09c7caec9b403ba685340844084738
ricofehr <ricofehr@nextdeploy.io>
2016-03-10 13:31:34 +0100

    Add java techno

---
+++ pm/manifests/java.pp
@@ -0,0 +1,33 @@
+# == Class: pm::java
+#
+# Install java with help of official module
+#
+# === Parameters
+#
+# [*version*]
+#   Version to install (6,7,8).
+#   Default: 7
+#
+# === Authors
+#
+# Eric Fehr <ricofehr@nextdeploy.io>
+#
+class pm::java ($version = '7') {
+  
+  # no java8 on ubuntu 14.04
+  if $::operatingsystem == 'Ubuntu' and $::lsbdistrelease == '14.04' and $version == '8' {
+    $version = '7'
+  }
+
+  # no java6 on debian 8
+  if $::operatingsystem == 'Debian' and versioncmp($::operatingsystemrelease, '8') >= 0 and $version == '6' {
+    $version = '7'
+  }
+
+  #rabbit setting
+  class { '::java':
+    distribution => 'jdk', 
+    version => $version
+  }
+
+}
