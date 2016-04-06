784ccfe132bb9f2c4fbbbe122f71dfffcaafb8cc
ricofehr <ricofehr@nextdeploy.io>
2016-04-06 10:04:44 +0200

    Add drush cim command tool

---
+++ pm/files/pm_tools/drupalcim.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# This script is executed as apache user, www-data, descend subversion
+
+E_BADRUN=1
+E_BADUSER=1
+E_BADDOMAIN=1
+E_BADARGS=65
+
+rc=0
+
+echo "########";
+    export LANG=fr_FR.UTF-8
+    echo "# Updating \"$1server\"";
+    cd $1/server
+    drush -y cim 2>&1
+    rc=$?;
+echo "########";
+
+# Exit
+exit $rc;
