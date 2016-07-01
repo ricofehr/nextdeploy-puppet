07d4fa02cca73dc03e5ece020658fbf2bc97b0d7
ricofehr <ricofehr@nextdeploy.io>
2016-07-01 18:18:36 +0200

    Add key to rabbit packages

--- pm/manifests/rabbit.pp
+++ pm/manifests/rabbit.pp
@@ -9,7 +9,9 @@
 #
 class pm::rabbit {
   #rabbit setting
-  class { 'rabbitmq': }
+  class { 'rabbitmq': 
+    package_gpg_key => "https://www.rabbitmq.com/rabbitmq-release-signing-key.asc"
+  }
 
   #create_resources ('rabbitmq_user', hiera('rabbitmq_user', []))
   #create_resources ('rabbitmq_user_permissions', hiera('rabbitmq_user_permissions', []))
