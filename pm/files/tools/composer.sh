6d1fddd67815744c9e312523edc7462cc4632886
ricofehr <ricofehr@nextdeploy.io>
2016-08-14 18:55:31 +0200

    Improve composersh script

--- pm/files/tools/composer.sh
+++ pm/files/tools/composer.sh
@@ -14,9 +14,9 @@ find . -maxdepth 6 -name composer.json | grep -v "vendor" | while read GFILE; do
   if [[ -f composer.lock ]] || [[ "$PWD" =~ ^.*/html.*$ ]]; then
     [[ -f composer.phar ]] && rm -f composer.phar
     curl -sS https://getcomposer.org/installer | php
-    /usr/bin/php composer.phar install -n --prefer-source
+    /usr/bin/php composer.phar install -n --prefer-dist --no-dev
     # if composer fails, sometimes is caused by github rates, sleep and try again
-    (( $? != 0 )) && sleep 20 && /usr/bin/php composer.phar install -n --prefer-source
+    (( $? != 0 )) && sleep 20 && /usr/bin/php composer.phar install -n --prefer-dist --no-dev
     rm -f composer.phar
   fi
   popd >/dev/null
