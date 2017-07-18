bfae4b1452b5a45be3231f2667c007cda6588e81
ricofehr <ricofehr@nextdeploy.io>
2017-07-18 14:15:24 +0200

    Fix api requests issue on nextdeploy local install

--- pm/manifests/deploy.pp
+++ pm/manifests/deploy.pp
@@ -315,7 +315,7 @@ class pm::deploy::postinstall {
   } ->
 
   exec { 'refreshcommit':
-    command => "test ${nextdeployuri} = nextdeploy.local || refreshcommit.sh ${nextdeployuri} ${vm_name}",
+    command => "refreshcommit.sh ${nextdeployuri} ${vm_name}  || test ${nextdeployuri} = nextdeploy.local",
     require => File['/usr/local/bin/refreshcommit.sh']
   }
 }
